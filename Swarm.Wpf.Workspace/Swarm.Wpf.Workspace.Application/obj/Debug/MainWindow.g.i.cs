10dfc96e864e011c6dedc610d4b706cdf68868b3
YOUR NAME <YOUR EMAIL ADDRESS>
2015-10-09 22:15:11 +0300

    PreparingSolution

--- Swarm.Wpf.Workspace/Swarm.Wpf.Workspace.Application/obj/Debug/MainWindow.g.i.cs
+++ Swarm.Wpf.Workspace/Swarm.Wpf.Workspace.Application/obj/Debug/MainWindow.g.i.cs
@@ -51,7 +51,7 @@ namespace Swarm.Wpf.Workspace {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Swarm.Wpf.Workspace;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Swarm.Wpf.Workspace.Application;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
