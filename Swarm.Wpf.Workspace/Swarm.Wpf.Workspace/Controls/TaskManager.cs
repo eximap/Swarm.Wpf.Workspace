9ccffad9d2b7d46acba72e48f0b98211003218a9
YOUR NAME <YOUR EMAIL ADDRESS>
2015-10-09 23:48:19 +0300

    d

---
+++ Swarm.Wpf.Workspace/Swarm.Wpf.Workspace/Controls/TaskManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Swarm.Wpf.Workspace.Controls
+{
+    /// <summary>
+    ///     Interaction logic for TaskManager.xaml
+    /// </summary>
+    public partial class TaskManager : UserControl
+    {
+        public TaskManager() { InitializeComponent(); }
+    }
+}
