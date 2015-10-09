c1100c009b6226def9df8a73d948dcee0a019ac4
YOUR NAME <YOUR EMAIL ADDRESS>
2015-10-09 22:18:27 +0300

    Preparing

---
+++ Swarm.Wpf.Workspace/Swarm.Wpf.Workspace.Application/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyDescription("Simple workspace on Wpf")]
+[assembly: AssemblyCopyright("Copyright © mamap 2015")]
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.*")]
+
+[assembly: AssemblyTitle("Swarm.Wpf.Workspace.Application")]
+[assembly: AssemblyProduct("Swarm.Wpf.Workspace.Application")]
+
+[assembly: ComVisible(false)]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
